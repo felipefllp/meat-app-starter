fea8e217bb7d24b5aef5f07c52e36109727d815b
Felipe Lopes <felipefllp@msn.com>
2019-09-09 05:41:12 -0300

    Detalhes de restaurantes e rotas filhas

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Restaurant} from './restaurant/restaurant.model'
+import { Restaurant } from './restaurant/restaurant.model'
 import { RestaurantsService } from './restaurants.service';
 
 @Component({
