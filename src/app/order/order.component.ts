6a05dc748be630d2876937c8c875d1b4bf475cce
Felipe Lopes <felipefllp@msn.com>
2019-09-12 00:12:52 -0300

    Página final e rating

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
+import { Order, OrderItem } from './order.model';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mt-order',
@@ -17,7 +20,7 @@ export class OrderComponent implements OnInit {
     {label: 'Cartão Refeição', value: 'REF'}
   ]
   
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -40,6 +43,20 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem){
     this.orderService.remove(item)
+  }
+
+  checkOrder(order: Order){
+    order.orderItems = this.cartItems()
+      .map( (item:CartItem) => new OrderItem(item.quantity, item.menuItem.id) )
+
+    this.orderService.checkOrder(order)
+    .subscribe( (orderId: string) => {
+      this.router.navigate(['/order-summary'])
+      console.log(`Compra concluída: ${orderId}`)
+      this.orderService.clear()
+    })
+
+    
   }
 
 }
