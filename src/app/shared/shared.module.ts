04257e95e9197b96b52dd821e076689e95b209f3
Felipe Lopes <felipefllp@msn.com>
2019-09-18 15:18:18 -0300

    Serviços adicionados ao shared module e pre-loading de módulos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule, ModuleWithProviders } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
@@ -6,6 +6,9 @@ import { InputComponent } from "./input/input.component";
 import { RadioComponent } from "./radio/radio.component";
 import { RatingComponent } from "./rating/rating.component";
 
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+import { OrderService } from "app/order/order.service";
 
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent],
@@ -13,4 +16,11 @@ import { RatingComponent } from "./rating/rating.component";
     exports: [InputComponent, RadioComponent, RatingComponent, CommonModule, FormsModule, ReactiveFormsModule]
 })
 
-export class SharedModule{}
+export class SharedModule{
+    static forRoot(): ModuleWithProviders {
+        return{
+            ngModule: SharedModule,
+            providers:[ShoppingCartService, RestaurantsService, OrderService]
+        }
+    }
+}
